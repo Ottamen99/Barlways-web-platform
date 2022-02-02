1d4851d3666ba30e0618d0059bda1b184ac57372
OttavioBuonomoUSI <91184854+OttavioBuonomoUSI@users.noreply.github.com>
2022-02-02 16:15:47 +0100

    Basic implementation of frontend

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
